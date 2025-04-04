926a195381a58a230864c59e7c1e5516100e7bb9
Jan <j.a.szumski@student.vu.nl>
2025-04-04 15:57:29 +0200

    formatting

--- Etch-A-Sketch.py
+++ Etch-A-Sketch.py
@@ -3,21 +3,27 @@ from turtle import Turtle, Screen
 crush = Turtle()
 screen = Screen()
 
+
 def move_forward():
     crush.forward(10)
+
 
 def move_backward():
     crush.backward(10)
 
+
 def rotate_counter_clockwise():
     crush.left(10)
+
 
 def rotate_clockwise():
     crush.right(10)
 
+
 def clear_and_center():
     crush.reset()
     crush.clear()
+
 
 screen.listen()
 screen.onkey(fun=move_forward, key="w")
